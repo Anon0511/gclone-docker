64413adecbc4aa812e865f7edc8567e7fb6fa33a
Anon0511 <kevin.reuber@gmail.com>
2023-01-05 15:29:45 +0100

    Fixes for cloud to cloud jobs

--- build/gclone_start.sh
+++ build/gclone_start.sh
@@ -9,9 +9,8 @@ fi
 
 # Variables
 
-FROM=$from_path
-TO=$to_path
 LOGFILE="/gclone/$(echo $to_path | cut -d: -f1)_$JOB.log"
+UPJOB=$(echo $JOB | tr '[:lower:]' '[:upper:]')
 
 if [ -z "$minage" ]
 then
@@ -19,16 +18,23 @@ then
   echo "setting minage : $intvl"
 fi
 
-# Check for files older than x
 
-if find $FROM* -type f -mmin +$MINAGE ! -name '*.!qB' | read
-  then
-  start=$(date +'%s')
-  echo "$(date "+%d.%m.%Y %T") RCLONE UPLOAD STARTED" | tee -a $LOGFILE
-  /usr/bin/gclone $JOB "$FROM" "$TO" --filter='- *.!qB' --min-age ${minage}m --log-level=INFO --log-file=$LOGFILE $OPTS
-  echo "$(date "+%d.%m.%Y %T") RCLONE UPLOAD FINISHED IN $(($(date +'%s') - $start)) SECONDS" | tee -a $LOGFILE
+if ! grep -Fxq $from_path /var/gclone/.config/rclone/rclone.conf
+then
+  if find $from_path -type f -mmin +$MINAGE ! -name '*.!qB' | read
+    then
+    start=$(date +'%s')
+    echo "$(date "+%d.%m.%Y %T") RCLONE $UPJOB STARTED" | tee -a $LOGFILE
+    /usr/bin/gclone $JOB "$from_path" "$to_path" --filter='- *.!qB' --min-age ${minage}m --log-level=INFO --log-file=$LOGFILE $OPTS
+    echo "$(date "+%d.%m.%Y %T") RCLONE $UPJOB FINISHED IN $(($(date +'%s') - $start)) SECONDS" | tee -a $LOGFILE
+  else
+    echo "Nothing to $JOB"
+  fi
 else
-  echo "Nothing to upload"
+  start=$(date +'%s')
+  echo "$(date "+%d.%m.%Y %T") RCLONE $UPJOB STARTED" | tee -a $LOGFILE
+  /usr/bin/gclone $JOB "$from_path" "$to_path" --filter='- *.!qB' --min-age ${minage}m --log-level=INFO --log-file=$LOGFILE $OPTS
+  echo "$(date "+%d.%m.%Y %T") RCLONE $UPJOB FINISHED IN $(($(date +'%s') - $start)) SECONDS" | tee -a $LOGFILE
 fi
 
 exit
